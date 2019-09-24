407a199debf2210ed57ac6a7006525af43952ba7
Melissa Kamrowski <kamro010@asimov.morris.umn.edu>
2019-09-24 11:33:22 -0500

    Begin writing disemvowel.c
    Create functions to recognize vowels, as well as determine final string length. Begin to put them together in the disemvowel function, does not work yet.

--- disemvowel/disemvowel.c
+++ disemvowel/disemvowel.c
@@ -2,6 +2,61 @@
 
 #include "disemvowel.h"
 
-char *disemvowel(char *str) {
-  return (char*) "";
+#include <cstring>
+
+
+int isVowel(char c){
+	if (c == 'a' || c == 'A' || c == 'e' || c == 'E' || c == 'i' || c == 'I'|| c == 'o' || c == 'O' || c == 'u' || c == 'U') {
+		return 1;
+	}
+	else
+	{
+		return 0;
+	}
 }
+
+
+int countConst(char str) {
+  
+  int i = 1;
+  
+  if (isVowel(str)){
+	continue;
+  }
+  else
+  { 
+	i += 1;
+  }
+  return i;
+}
+
+
+
+char *disemvowel(char *str) {
+
+  char full[] = str
+  char new[countConst(str) + 1]
+  int c = 0;
+
+
+  for (c; full[c] != '\0'; c++) {
+	  //checks if the char at c is a vowel
+	if (isVowel(full[c])) {
+		continue;
+	}
+	else 
+  	{
+           //append const to array
+	  new[] == strcat(new[], c);
+	}
+  return new[];
+  }
+
+  return (char*) new[];
+}
+
+
+
+
+
+
