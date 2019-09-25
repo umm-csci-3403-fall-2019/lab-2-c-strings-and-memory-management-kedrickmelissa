e1b1e6532a75f0dc873e526df44f303ff44511fc
hill1588 <hill1588@morris.umn.edu>
2019-09-24 20:50:33 -0500

    fix issue where errors were preventing compliation
    added function isVowel to the header file, adjust disemvowel.c to make it more simple, started free memory from main.c

--- disemvowel/disemvowel.c
+++ disemvowel/disemvowel.c
@@ -1,7 +1,33 @@
 #include <stdlib.h>
-
+#include <stdio.h>
+#include <string.h>
 #include "disemvowel.h"
 
 char *disemvowel(char *str) {
-  return (char*) "";
+	char *nStr;
+	int i = 0, c;
+
+	for (i = 0; str[i] != '\0'; i++) {
+		if(isVowel(str[i]) == 1) {
+			c++;
+		}
+	}
+	 nStr = (char*)calloc(c+1, sizeof(char));
+
+	 for(int j = 0, k = 0; j < i; j++) {
+		 if(isVowel(str[j]) == 0) {
+			 nStr[k] = str[j];
+			 k++;
+		 }
+	 }
+	 nStr[c] = '\0';
+
+	return (char*) nStr;
+}
+
+int isVowel(char c){
+	if (c == 'a' || c == 'A' || c == 'e' || c == 'E' || c == 'i' || c == 'I'|| c == 'o' || c == 'O' || c == 'u' || c == 'U') {
+	       return 1;
+	}
+return 0;	
 }
